c1936b897eeba16e4edb971530cc7de24911762b
Cyrus Sethna <csethna@gmail.com>
2017-12-20 13:40:52 -0500

    dataframe

--- maumee.py
+++ maumee.py
@@ -1,5 +1,6 @@
 import pandas as pd # imports Pandas datascience library as 'pd'
 import os # imports os library to perform system functions such as opening files
+import datetime #imports datetime library for boolean mask
 
 # Specify the data source
 CSV_PATH = os.path.join('maumee_annual_sums.csv')
@@ -8,4 +9,9 @@ df = pd.read_csv(CSV_PATH)
 
 df['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
 
-df['Date'] = pd.to_datetime(df['Date']) # check "Date" to ensure data is a series
+df['Date'] = pd.to_datetime(df['Date']) # check "Date" to ensure data is a series type
+
+start_date = df('Date')
+end_date =
+
+mask = (df['Date'] > start_date) & (df['Date'] <= end_date)
