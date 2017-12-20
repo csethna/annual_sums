54e8d1481aede7b3c89c936ef8ea1b485f727499
Cyrus Sethna <csethna@gmail.com>
2017-12-20 13:37:54 -0500

    working in the dataframe

--- maumee.py
+++ maumee.py
@@ -1,1 +1,11 @@
-import pandas as pd
+import pandas as pd # imports Pandas datascience library as 'pd'
+import os # imports os library to perform system functions such as opening files
+
+# Specify the data source
+CSV_PATH = os.path.join('maumee_annual_sums.csv')
+# Create the DataFrame
+df = pd.read_csv(CSV_PATH)
+
+df['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
+
+df['Date'] = pd.to_datetime(df['Date']) # check "Date" to ensure data is a series
