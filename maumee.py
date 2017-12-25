5c4e83c05780ca6aad8a4a3f762c96f155cfe75c
Cyrus Sethna <csethna@gmail.com>
2017-12-25 18:28:59 -0500

    creating dataframe

--- maumee.py
+++ maumee.py
@@ -7,7 +7,8 @@ import datetime #imports datetime library for boolean mask
 # Specify the data source
 CSV_PATH = os.path.join('maumee_annual_sums.csv')
 # Create the DataFrame
-df = pd.read_csv(CSV_PATH, index_col='Date')
+df = pd.read_csv(CSV_PATH, names=['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)'], parse_dates='Date', index_col='Date')
+# df = pd.read_csv(CSV_PATH, index_col='Date')
 
 df['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
 
