867c982b902a7a58f61f00e277c84c5c32d85c79
Cyrus Sethna <csethna@gmail.com>
2017-12-25 18:23:16 -0500

    maumee datascience project

--- maumee.py
+++ maumee.py
@@ -1,11 +1,13 @@
 import pandas as pd # imports Pandas datascience library as 'pd'
+import numpy as np
+import matplotlib as plt
 import os # imports os library to perform system functions such as opening files
 import datetime #imports datetime library for boolean mask
 
 # Specify the data source
 CSV_PATH = os.path.join('maumee_annual_sums.csv')
 # Create the DataFrame
-df = pd.read_csv(CSV_PATH)
+df = pd.read_csv(CSV_PATH, index_col='Date')
 
 df['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
 
