cefa7eab031ebf45599cd42c6d570fec7137f03f
Cyrus Sethna <csethna@gmail.com>
2017-12-25 21:40:32 -0500

    cleanup

--- maumee.py
+++ maumee.py
@@ -1,9 +1,5 @@
 import pandas as pd # imports Pandas datascience library as 'pd'
-import numpy as np
-import matplotlib as plt
 import os # imports os library to perform system functions such as opening files
-from datetime import datetime, date #imports datetime library for boolean mask
-import tabulate as tabulate
 
 # Specify the data source
 CSV_PATH = os.path.join('maumee_annual_sums.csv')
@@ -17,19 +13,10 @@ df = pd.read_csv(CSV_PATH, parse_dates=['Date'], index_col=0)
 # Create pivot table to focus on relevant columns
 p = df.pivot_table(index=['Date'])
 # Use reindex to make the 'Date' column the index column
-columns = ['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
+columns = ['Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
 p = p.reindex(columns=columns)
-p[columns] = p[columns].astype(str) # assigns type to string
+p[columns] = p[columns].astype(float) # assigns type to string
 df = p
-
-# Delete the empty date column
-del df['Date']
-
-# convert values to float64
-df['Maumee TP Load (tonnes)'] = df['Maumee TP Load (tonnes)'].astype(str).astype(float)
-df['Maumee SRP Load (tonnes)'] = df['Maumee SRP Load (tonnes)'].astype(str).astype(float)
-df['Maumee TN Load (tonnes)'] = df['Maumee TN Load (tonnes)'].astype(str).astype(float)
-df['Maumee Silica Load (tonnes)'] = df['Maumee Silica Load (tonnes)'].astype(str).astype(float)
 
 # Define Date Range
 annual_range = df.loc['2000-1-1' : '2000-12-31']
@@ -41,16 +28,3 @@ si_total_range = annual_range['Maumee Silica Load (tonnes)'].sum()
 # Totals for Range
 print("For the specified annual range: \n"
 'Maumee TP Load (tonnes) = ', tp_total_range, "\n", 'Maumee SRP Load (tonnes) = ', srp_total_range, "\n", 'Maumee TN Load (tonnes) = ', tn_total_range, "\n", 'Maumee Silica Load (tonnes) = ', si_total_range)
-
-# pretty print DataFrame
-print(tabulate(df, headers='keys', tablefmt='psql'))
-
-#
-# df['Date', 'Maumee TP Load (tonnes)', 'Maumee SRP Load (tonnes)', 'Maumee TN Load (tonnes)', 'Maumee Silica Load (tonnes)']
-#
-# df['Date'] = pd.to_datetime(df['Date']) # check "Date" to ensure data is a series type
-#
-# start_date = df('Date')
-# end_date =
-#
-# mask = (df['Date'] > start_date) & (df['Date'] <= end_date)
